f153f0de2918ba60e83aa1b1ee164c557de915b8
johndy <johndypanca12345@gmail.com>
2023-05-30 20:07:39 +0700

    upload

---
+++ app/src/main/java/com/example/utspraktik_if10k_10120905_johndypanca/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.utspraktik_if10k_10120905_johndypanca
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
